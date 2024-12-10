0baeafc8a872078326a78fc1ad22dda1fee4ee3a
lusmila13 <lusmitrini2006@gmail.com>
2024-12-10 10:30:43 -0300

    no usAAAAr

---
+++ constants/recordsMundiales.ts
@@ -0,0 +1,274 @@
+export const RECORDS_MUNDIALES = [
+  {
+    titulo: "Edificio más alto",
+    categoria: "Arquitectura",
+    duenioTitulo: "Burj Khalifa",
+    logro: "828 metros de altura",
+    fechaObtenido: "2010-01-04",
+    lugar: "Dubái, EAU"
+  },
+  {
+    titulo: "Carrera de 100m más rápida (humano)",
+    categoria: "Deportes",
+    duenioTitulo: "Usain Bolt",
+    logro: "9.58 segundos",
+    fechaObtenido: "2009-08-16",
+    lugar: "Berlín, Alemania"
+  },
+  {
+    titulo: "Mayor tiempo de vida humana",
+    categoria: "Cuerpo Humano",
+    duenioTitulo: "Jeanne Calment",
+    logro: "122 años, 164 días",
+    fechaObtenido: "1997-08-04",
+    lugar: "Arlés, Francia"
+  },
+  {
+    titulo: "Pizza más grande jamás hecha",
+    categoria: "Comida",
+    duenioTitulo: "Pizza Factory",
+    logro: "1,261.65 metros cuadrados",
+    fechaObtenido: "2012-12-13",
+    lugar: "Roma, Italia"
+  },
+  {
+    titulo: "Aeronave más pesada",
+    categoria: "Tecnología",
+    duenioTitulo: "Antonov An-225 Mriya",
+    logro: "640,000 kg",
+    fechaObtenido: "1988-12-21",
+    lugar: "Kiev, Ucrania"
+  },
+  {
+    titulo: "Cabello más largo (mujer)",
+    categoria: "Cuerpo Humano",
+    duenioTitulo: "Xie Qiuping",
+    logro: "5.627 metros",
+    fechaObtenido: "2004-05-08",
+    lugar: "Guangxi, China"
+  },
+  {
+    titulo: "Pastel más grande jamás horneado",
+    categoria: "Comida",
+    duenioTitulo: "Southern Bakers Collective",
+    logro: "25,000 kg",
+    fechaObtenido: "2010-11-20",
+    lugar: "Los Ángeles, EE. UU."
+  },
+  {
+    titulo: "Animal terrestre más rápido",
+    categoria: "Naturaleza",
+    duenioTitulo: "Guepardo",
+    logro: "120 km/h",
+    fechaObtenido: "2011-06-15",
+    lugar: "Serengeti, Tanzania"
+  },
+  {
+    titulo: "Rueda de la fortuna más grande",
+    categoria: "Ingeniería",
+    duenioTitulo: "Ain Dubai",
+    logro: "250 metros de altura",
+    fechaObtenido: "2021-10-21",
+    lugar: "Dubái, EAU"
+  },
+  {
+    titulo: "Más hotdogs comidos en 10 minutos",
+    categoria: "Comida",
+    duenioTitulo: "Joey Chestnut",
+    logro: "76 hotdogs",
+    fechaObtenido: "2021-07-04",
+    lugar: "Nueva York, EE. UU."
+  },
+  {
+    titulo: "Maratón de videojuegos más larga",
+    categoria: "Videojuegos",
+    duenioTitulo: "Okan Kaya",
+    logro: "138 horas",
+    fechaObtenido: "2012-11-18",
+    lugar: "Sídney, Australia"
+  },
+  {
+    titulo: "Mayor reunión de Santa Claus",
+    categoria: "Eventos",
+    duenioTitulo: "Ciudad de Derry",
+    logro: "13,000 Santas",
+    fechaObtenido: "2007-12-09",
+    lugar: "Derry, Irlanda del Norte"
+  },
+  {
+    titulo: "Hombre vivo más alto",
+    categoria: "Cuerpo Humano",
+    duenioTitulo: "Sultan Kösen",
+    logro: "2.51 metros",
+    fechaObtenido: "2009-09-17",
+    lugar: "Ankara, Turquía"
+  },
+  {
+    titulo: "Uñas más largas de la historia",
+    categoria: "Cuerpo Humano",
+    duenioTitulo: "Ayanna Williams",
+    logro: "733.55 cm (longitud combinada)",
+    fechaObtenido: "2021-04-07",
+    lugar: "Houston, EE. UU."
+  },
+  {
+    titulo: "Pintura más cara vendida",
+    categoria: "Arte",
+    duenioTitulo: "Salvator Mundi de Leonardo da Vinci",
+    logro: "$450.3 millones",
+    fechaObtenido: "2017-11-15",
+    lugar: "Nueva York, EE. UU."
+  },
+  {
+    titulo: "Puente más largo (tramo continuo)",
+    categoria: "Ingeniería",
+    duenioTitulo: "Gran Puente Danyang-Kunshan",
+    logro: "164.8 kilómetros",
+    fechaObtenido: "2011-06-30",
+    lugar: "Jiangsu, China"
+  },
+  {
+    titulo: "Más dominadas en 24 horas",
+    categoria: "Fitness",
+    duenioTitulo: "John Orth",
+    logro: "7,715 dominadas",
+    fechaObtenido: "2020-11-01",
+    lugar: "Colorado, EE. UU."
+  },
+  {
+    titulo: "Vuelo continuo más largo (ave)",
+    categoria: "Naturaleza",
+    duenioTitulo: "Aguja colipinta",
+    logro: "12,200 km",
+    fechaObtenido: "2020-10-23",
+    lugar: "Océano Pacífico"
+  },
+  {
+    titulo: "Copo de nieve más grande",
+    categoria: "Naturaleza",
+    duenioTitulo: "Rancho Fort Keogh",
+    logro: "38 cm de ancho",
+    fechaObtenido: "1887-01-28",
+    lugar: "Montana, EE. UU."
+  },
+  {
+    titulo: "Río más largo",
+    categoria: "Naturaleza",
+    duenioTitulo: "Río Nilo",
+    logro: "6,650 kilómetros",
+    fechaObtenido: "2021-08-10",
+    lugar: "Egipto"
+  },
+  {
+    titulo: "Barra de chocolate más grande",
+    categoria: "Comida",
+    duenioTitulo: "Thorntons PLC",
+    logro: "5,792 kg",
+    fechaObtenido: "2011-10-07",
+    lugar: "Derbyshire, Reino Unido"
+  },
+  {
+    titulo: "Montaña rusa más rápida",
+    categoria: "Ingeniería",
+    duenioTitulo: "Formula Rossa",
+    logro: "240 km/h",
+    fechaObtenido: "2010-11-04",
+    lugar: "Abu Dabi, EAU"
+  },
+  {
+    titulo: "Mayor colección de osos de peluche",
+    categoria: "Coleccionables",
+    duenioTitulo: "Jackie Miley",
+    logro: "8,025 osos",
+    fechaObtenido: "2012-09-10",
+    lugar: "Hill City, EE. UU."
+  },
+  {
+    titulo: "Más tatuajes en 24 horas",
+    categoria: "Cuerpo Humano",
+    duenioTitulo: "Hollis Cantrell",
+    logro: "801 tatuajes",
+    fechaObtenido: "2008-11-08",
+    lugar: "Phoenix, EE. UU."
+  },
+  {
+    titulo: "Reacción en cadena de dominó más larga",
+    categoria: "Eventos",
+    duenioTitulo: "Equipo de Dominó",
+    logro: "4,491,863 fichas",
+    fechaObtenido: "2009-11-13",
+    lugar: "Países Bajos"
+  },
+  {
+    titulo: "Mayor colección de videojuegos",
+    categoria: "Coleccionables",
+    duenioTitulo: "Antonio Monteiro",
+    logro: "20,139 juegos",
+    fechaObtenido: "2019-03-01",
+    lugar: "Richmond, EE. UU."
+  },
+  {
+    titulo: "Calabaza más pesada",
+    categoria: "Agricultura",
+    duenioTitulo: "Mathias Willemijns",
+    logro: "1,190.5 kg",
+    fechaObtenido: "2016-10-09",
+    lugar: "Ludwigsburg, Alemania"
+  },
+  {
+    titulo: "Mayor tiempo aguantando la respiración (bajo agua)",
+    categoria: "Cuerpo Humano",
+    duenioTitulo: "Aleix Segura Vendrell",
+    logro: "24 minutos, 3 segundos",
+    fechaObtenido: "2016-02-28",
+    lugar: "Barcelona, España"
+  },
+  {
+    titulo: "Castillo de arena más grande",
+    categoria: "Arquitectura",
+    duenioTitulo: "Skulptura Projects",
+    logro: "17.66 metros de altura",
+    fechaObtenido: "2019-07-06",
+    lugar: "Binz, Alemania"
+  },
+  {
+    titulo: "Más personas en una tabla de surf",
+    categoria: "Deportes",
+    duenioTitulo: "Comunidad de Surf",
+    logro: "66 personas",
+    fechaObtenido: "2015-06-20",
+    lugar: "Huntington Beach, EE. UU."
+  },
+  {
+    titulo: "Concierto de música más largo",
+    categoria: "Música",
+    duenioTitulo: "Club de Música de Montreal",
+    logro: "453 horas",
+    fechaObtenido: "2017-07-03",
+    lugar: "Montreal, Canadá"
+  },
+  {
+    titulo: "Mayor colección de objetos de Star Wars",
+    categoria: "Coleccionables",
+    duenioTitulo: "Steve Sansweet",
+    logro: "500,000+ objetos",
+    fechaObtenido: "2015-11-14",
+    lugar: "Petaluma, EE. UU."
+  },
+  {
+    titulo: "Parada de manos más larga",
+    categoria: "Fitness",
+    duenioTitulo: "Yuji Yamamoto",
+    logro: "3 horas, 44 minutos",
+    fechaObtenido: "2019-09-12",
+    lugar: "Tokio, Japón"
+  },
+  {
+    titulo: "1,000 km más rápido en bicicleta",
+    categoria: "Deportes",
+    duenioTitulo: "Christoph Strasser",
+    logro: "28 horas, 20 minutos",
+    fechaObtenido: "2020-08-22",
+    lugar: "Austria"
+  }
+];
---
+++ servidor.ts
@@ -0,0 +1,3 @@
+// Implementar dos rutas API:
+// GET /records - Obtiene todos los records registrados.
+// GET /records/:categoria - Obitene todos los records para la categoría dada.
