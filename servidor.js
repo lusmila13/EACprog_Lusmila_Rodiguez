b0338a163e42d35e53b781f0f2069853a121fd03
lusmila13 <lusmitrini2006@gmail.com>
2024-12-11 09:44:50 -0300

    disney

--- servidor.js
+++ servidor.js
@@ -2,11 +2,16 @@ const express = require('express');
 const app = express();
 app.get('/', (_, res) => {
   let bio ={
-    'titulo' : 'Spiderman a Traves del SpiderVerso',
-    'puntuación': '10/10',
-    'opinion': 'Spderman a Traves dele SpiderVerso se trata de cuando a Miles Morales es mordido por una araña radiactiva, luego Miles descubre qque su tío es  un villano, el mismo que quire ver a Spderman muerto, pero llega un día en el que Aaron su tío lo descubre, pero no le importa hy decide continuar con la misión, pero al fin de  cuenta el que muere es  el tío, y toda la ciudad de Brooklyn, Nuea York piensa que murio por culpa de Spiderman'
+    'Titulo de  la reseña' : 'Spiderman a Traves del SpiderVerso',
+    'Puntuación': '10/10',
+    'Opinion': 'Spderman a Traves dele SpiderVerso se trata de cuando a Miles Morales es mordido por una araña radiactiva, luego Miles descubre qque su tío es  un villano, el mismo que quire ver a Spderman muerto, pero llega un día en el que Aaron su tío lo descubre, pero no le importa hy decide continuar con la misión, pero al fin de  cuenta el que muere es  el tío, y toda la ciudad de Brooklyn, Nuea York piensa que murio por culpa de Spiderman'
   }
-  res.send(bio);
+  let Disney ={
+    'Titulo de la reseña' : 'Disney',
+    'Puntuación' : '9.5/10',
+    'Opinion' : 'Diney es un programa de televisión desde hace 101 años, hace una decada que lo hermanos Walt y Roy Disney fundaron esta conpañia, hoy en día en 2024 se dio la triste noticia de que la compañia Disney cerrara definitivamente'
+  }
+  res.send([bio, Disney]);
 });
 
 app.get('/:nombre', (req, res) => {
